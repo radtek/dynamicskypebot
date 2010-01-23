496a2acb33269b413e57c9df7af1273708da7a55
Eckankar <Eckankar@d44c2b64-5b73-11de-8abc-d1c337b90d21>
2010-01-23 12:29:19 +0000

    Beginning of classes for mazing.

---
+++ branches/maze/SkypeBot/plugins/maze/Maze.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkypeBot.plugins.maze {
+    [Serializable]
+    class Maze {
+        private MazeCell[,] cells;
+
+        protected Maze(int width, int height) {
+            cells = new MazeCell[width, height];
+
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    cells[x, y] = new MazeCell(true);
+                }
+            }
+
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    if (x > 0)
+                        cells[x, y].West.OtherSide = cells[x - 1, y];
+                    if (x < width - 1)
+                        cells[x, y].East.OtherSide = cells[x + 1, y];
+                    if (y > 0)
+                        cells[x, y].North.OtherSide = cells[x, y - 1];
+                    if (y < height - 1)
+                        cells[x, y].South.OtherSide = cells[x, y + 1];
+                }
+            }
+        }
+    }
+}
---
+++ branches/maze/SkypeBot/plugins/maze/MazeLink.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkypeBot.plugins.maze {
+    [Serializable]
+    class MazeLink {
+        MazeCell otherSide;
+        bool wall;
+
+        public MazeCell OtherSide {
+            get { return otherSide; }
+            set { otherSide = value; }
+        }
+
+        public Boolean Wall {
+            get { return wall; }
+            set { wall = value; }
+        }
+
+        public MazeLink(bool walled) {
+            wall = walled;
+        }
+    }
+}
