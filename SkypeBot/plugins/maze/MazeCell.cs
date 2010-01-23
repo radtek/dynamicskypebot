4007c7184866e5f3abd01598b054ca614e5fb43b
Eckankar <Eckankar@d44c2b64-5b73-11de-8abc-d1c337b90d21>
2010-01-23 12:29:19 +0000

    Beginning of classes for mazing.

---
+++ SkypeBot/plugins/maze/MazeCell.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkypeBot.plugins.maze {
+    [Serializable]
+    class MazeCell {
+        private MazeLink north, west, south, east;
+
+        private bool hasDown, seen;
+
+        public MazeLink North {
+            get { return north; }
+            set { north = value; }
+        }
+
+        public MazeLink West {
+            get { return west; }
+            set { west = value; }
+        }
+
+        public MazeLink South {
+            get { return south; }
+            set { south = value; }
+        }
+
+        public MazeLink East {
+            get { return east; }
+            set { east = value; }
+        }
+
+        public Boolean HasDown {
+            get { return hasDown; }
+            set { hasDown = value; }
+        }
+
+        public Boolean Seen {
+            get { return seen; }
+            set { seen = value; }
+        }
+
+        public MazeCell(bool walled) {
+            north = new MazeLink(walled);
+            west = new MazeLink(walled);
+            east = new MazeLink(walled);
+            south = new MazeLink(walled);
+
+            hasDown = false;
+            seen = false;
+        }
+
+    }
+}
---
+++ SkypeBot/plugins/maze/MazeFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkypeBot.plugins.maze {
+    class MazeFactory {
+        private static const Random random = new Random();
+
+        private MazeFactory() { }
+
+    }
+}
